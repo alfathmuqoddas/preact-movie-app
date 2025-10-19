03792d6397fa5f6ae882408ed5508d7713f009da
alfathmuqoddas <85182650+alfathmuqoddas@users.noreply.github.com>
2025-10-19 13:01:29 +0000

    Deploy build to dev

---
+++ assets/index-CcpfRSWD.js
@@ -0,0 +1,1 @@
+import{u as t,k as o}from"./index-trzrdXid.js";function u(){return t(o,{children:"About"})}export{u as default};
---
+++ assets/index-DkXFObYp.js
@@ -0,0 +1,1 @@
+import{u as r}from"./index-trzrdXid.js";function n(){return r("div",{children:"Search Component"})}export{n as default};
