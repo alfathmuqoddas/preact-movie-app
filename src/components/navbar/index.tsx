a5afa72e1dd5a34451f7033667c8b924b3a6c4ae
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2025-10-19 20:12:00 +0700

    update: add inter font

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,9 +3,9 @@ export default function Navbar() {
   return (
     <nav className="font-semibold mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 mt-1">
       <div className="rounded-full bg-blue-300 flex  px-4 sm:px-6 lg:px-8 items-center gap-4 h-12">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/Search">Search</Link>
+        <Link to="/preact-movie-app/">Home</Link>
+        <Link to="/preact-movie-app/about">About</Link>
+        <Link to="/preact-movie-app/search">Search</Link>
       </div>
     </nav>
   );
